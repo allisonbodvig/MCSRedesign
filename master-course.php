9bc1f592e3f42975e047c114882c5640fa433327
zjpierson <zachary.pierson@mines.sdsmt.edu>
2016-04-13 16:53:02 -0600

    template for main and master course pages

---
+++ master-course.php
@@ -0,0 +1,21 @@
+<!-- 
+                    ****master-course.php****
+
+    This is the main page for both the math and computer science courses
+
+    Authors: Forrest Miller, Zachary Pierson, Allison Bodvig
+    Class: CSC 468 (GUI)
+    Proffesor: Dr. John Weiss
+ -->
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>MCS Courses</title>
+  </head>
+  <body>
+    <h1>Math and Computer Science Course Home Page</h1>
+    <p>add course things...</p>
+  </body>
+
+</html>
