60df9895ad365b8dc9a980cc4c9b1bff8951a06a
zjpierson <zachary.pierson@mines.sdsmt.edu>
2016-04-13 18:10:19 -0600

    initial design

---
+++ csc-courses.php
@@ -0,0 +1,21 @@
+<!-- 
+                    ****csc-courses.php****
+
+    This is the main page for both the math and computer science courses
+
+    Authors: Forrest Miller, Zachary Pierson, Allison Bodvig
+    Class: CSC 468 (GUI)
+    Proffesor: Dr. John Weiss
+ -->
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Computer Science Courses</title>
+  </head>
+  <body>
+    <h1> Computer Science Courseses</h1>
+    <p>add course things...</p>
+  </body>
+
+</html>
--- math-courses.php
+++ math-courses.php
@@ -1,7 +1,7 @@
 <!-- 
-                    ****master-course.php****
+                    ****math-courses.php****
 
-    This is the main page for both the math and computer science courses
+    This is the main page for math courses
 
     Authors: Forrest Miller, Zachary Pierson, Allison Bodvig
     Class: CSC 468 (GUI)
@@ -11,10 +11,10 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>MCS Courses</title>
+    <title>Computer Science Courses</title>
   </head>
   <body>
-    <h1>Math and Computer Science Course Home Page</h1>
+    <h1> Math Courseses</h1>
     <p>add course things...</p>
   </body>
 
