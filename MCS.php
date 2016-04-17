cb720c406c9cf51b53768b5656fb2ed1eb6e1eb6
zjpierson <zachary.pierson@mines.sdsmt.edu>
2016-04-17 12:51:42 -0600

    Main website design

--- MCS.php
+++ MCS.php
@@ -19,58 +19,69 @@
   <body>
     <img src="math-csc-logo.png">
     </ br>
-    <ul class="menu">
-      <div class="menuTitle">MCS Menu</div>
-      <hr class="menuDivider">
-      <li>
-        <a class="menuItem" href="csc-courses.php">Computer Science Courses</a>
-      </li>
-      <li>
-        <a class="menuItem" href="math-courses.php">Math Courses</a>
-      </li>
-      <li>
-        <a class="menuItem" href="empty-redirect.html">Faculty</a>
-      </li>
-      <li>
-        <a class="menuItem" href="empty-redirect.html">Students</a>
-      </li>
-      <li>
-        <a class="menuItem" href="empty-redirect.html">Alumni</a>
-      </li>
-      <li>
-        <a class="menuItem" href="empty-redirect.html">Departmental Directory</a>
-      </li>
-      <li>
-        <a class="menuItem" href="empty-redirect.html">Building Map</a>
-      </li>
-      <li>
-        <a class="menuItem" href="empty-redirect.html">CS Checklist</a>
-      </li>
-      <li>
-        <a class="menuItem" href="empty-redirect.html">CS Flowchart</a>
-      </li>
-      <li>
-        <a class="menuItem" href="empty-redirect.html">CS Course Schedualer</a>
-      </li>
-      <li>
-        <a class="menuItem" href="empty-redirect.html">MCS Colloquium</a>
-      </li>
-      <li>
-        <a class="menuItem" href="empty-redirect.html">MCS Research</a>
-      </li>
-      <li>
-        <a class="menuItem" href="empty-redirect.html">Student Organizations</a>
-      </li>
-      <li>
-        <a class="menuItem" href="empty-redirect.html">Tutorials and Resources</a>
-      </li>
-      <li>
-        <a class="menuItem" href="empty-redirect.html">Policy, Forms, Coding Standards</a>
-      </li>
-      <li>
-        <a class="menuItem" href="empty-redirect.html">Submit It!</a>
-      </li>
-    </ul>
+    <div>
+      <ul class="menu">
+        <div class="menuTitle">MCS Menu</div>
+        <hr class="menuDivider">
+        <li>
+          <a class="menuItem" href="csc-courses.php">Computer Science Courses</a>
+        </li>
+        <li>
+          <a class="menuItem" href="math-courses.php">Math Courses</a>
+        </li>
+        <li>
+          <a class="menuItem" href="empty-redirect.html">Faculty</a>
+        </li>
+        <li>
+          <a class="menuItem" href="empty-redirect.html">Students</a>
+        </li>
+        <li>
+          <a class="menuItem" href="empty-redirect.html">Alumni</a>
+        </li>
+        <li>
+          <a class="menuItem" href="empty-redirect.html">Departmental Directory</a>
+        </li>
+        <li>
+          <a class="menuItem" href="empty-redirect.html">Building Map</a>
+        </li>
+        <li>
+          <a class="menuItem" href="empty-redirect.html">CS Checklist</a>
+        </li>
+        <li>
+          <a class="menuItem" href="empty-redirect.html">CS Flowchart</a>
+        </li>
+        <li>
+          <a class="menuItem" href="empty-redirect.html">CS Course Schedualer</a>
+        </li>
+        <li>
+          <a class="menuItem" href="empty-redirect.html">MCS Colloquium</a>
+        </li>
+        <li>
+          <a class="menuItem" href="empty-redirect.html">MCS Research</a>
+        </li>
+        <li>
+          <a class="menuItem" href="empty-redirect.html">Student Organizations</a>
+        </li>
+        <li>
+          <a class="menuItem" href="empty-redirect.html">Tutorials and Resources</a>
+        </li>
+        <li>
+          <a class="menuItem" href="empty-redirect.html">Policy, Forms, Coding Standards</a>
+        </li>
+        <li>
+          <a class="menuItem" href="empty-redirect.html">Submit It!</a>
+        </li>
+      </ul>
+    </div>
+
+    <img src="McLaury.jpg" width="40%" style="float:left;margin:63px 20px 10px 0px;">
+
+    <p style="margin:100px 10px 10px 0px;">Welcome to Mathematics and Computer Science @ SDSM&T
+  We are in the process of moving all our materials to the main campus website. You can find the new department home at http://www.sdsmt.edu/MCS/.
+  Our department offers a B.S. in Computer Science, a B.S. in Applied and Computational Mathematics, and a new M.S. in Computational Sciences and Robotics. Our B.S. Computer Science degree is accredited by the Computing Accreditation Commission of ABET, http://www.abet.org.</p>
+    <p>A Few Items Still on this Website
+The code submission page has been redesigned to hopefully make it more user friendly and intuitive. The code submission page can now be found under "Students Submit It!". The Alumni, resources, and policy pages on the left navigation menu are still complete with the information we have been maintaining. Please check out the new department home at http://www.sdsmt.edu/MCS/ to get the latest and greatest information.</p>
+
   </body>
 
 </html>
