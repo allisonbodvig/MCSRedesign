a4cc523a8d6166e50f6ba34772cd7446459d0ae4
zjpierson <zachary.pierson@mines.sdsmt.edu>
2016-04-17 10:58:59 -0600

    adding redirect

---
+++ MCS.php
@@ -0,0 +1,76 @@
+<!-- 
+                    ****MCS.html****
+
+    This is the starting point for the MCS Home page redesign.
+    development branch is on the mcs server at dev.mcs.sdsmt.edu/~<StudentId>
+    Where <StudentId> is either 7103025, 7182112, 7213465.
+
+    Authors: Forrest Miller, Zachary Pierson, Allison Bodvig
+    Class: CSC 468 (GUI)
+    Proffesor: Dr. John Weiss
+ -->
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Home</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <img src="math-csc-logo.png">
+    </ br>
+    <ul class="menu">
+      <div class="menuTitle">MCS Menu</div>
+      <hr class="menuDivider">
+      <li>
+        <a class="menuItem" href="csc-courses.php">Computer Science Courses</a>
+      </li>
+      <li>
+        <a class="menuItem" href="math-courses.php">Math Courses</a>
+      </li>
+      <li>
+        <a class="menuItem" href="empty-redirect.html">Faculty</a>
+      </li>
+      <li>
+        <a class="menuItem" href="empty-redirect.html">Students</a>
+      </li>
+      <li>
+        <a class="menuItem" href="empty-redirect.html">Alumni</a>
+      </li>
+      <li>
+        <a class="menuItem" href="empty-redirect.html">Departmental Directory</a>
+      </li>
+      <li>
+        <a class="menuItem" href="empty-redirect.html">Building Map</a>
+      </li>
+      <li>
+        <a class="menuItem" href="empty-redirect.html">CS Checklist</a>
+      </li>
+      <li>
+        <a class="menuItem" href="empty-redirect.html">CS Flowchart</a>
+      </li>
+      <li>
+        <a class="menuItem" href="empty-redirect.html">CS Course Schedualer</a>
+      </li>
+      <li>
+        <a class="menuItem" href="empty-redirect.html">MCS Colloquium</a>
+      </li>
+      <li>
+        <a class="menuItem" href="empty-redirect.html">MCS Research</a>
+      </li>
+      <li>
+        <a class="menuItem" href="empty-redirect.html">Student Organizations</a>
+      </li>
+      <li>
+        <a class="menuItem" href="empty-redirect.html">Tutorials and Resources</a>
+      </li>
+      <li>
+        <a class="menuItem" href="empty-redirect.html">Policy, Forms, Coding Standards</a>
+      </li>
+      <li>
+        <a class="menuItem" href="empty-redirect.html">Submit It!</a>
+      </li>
+    </ul>
+  </body>
+
+</html>
---
+++ README.txt
@@ -0,0 +1,30 @@
+                MCS Website Redesign
+                  April 27, 2015
+
+Institution: South Dakota School of Mines and Technology
+Authors: Allison Bodvig, Forrest Miller, & Zachary Pierson
+Instructor: Dr. John Weiss
+Class: CSC468 - Graphical User Interface
+
+
+
+CONTENTS OF THIS FILE
+---------------------
+    I. Introduction
+   II. Requirements
+  III. Installation
+   IV. References
+
+
+I. INTRODUCTION
+---------------
+The MCS departmental website was designed many years ago. The interface is 
+dated, and much of the original content has been moved to the main SDSM&T
+website. It is high time for a departmental website redesign, along with 
+the addition of new functionality.
+
+
+II. REQUIREMENTS
+----------------
+
+Repository: https://github.com/allisonbodvig/MCSRedesign.git
