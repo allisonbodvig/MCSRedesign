205f08754cb0d58dc51651dbe92a705487fabdf1
Allison Bodvig <7213465@linux02.mcs.sdsmt.edu>
2016-04-20 16:10:12 -0600

    courses printing to screen

--- math-courses.php
+++ math-courses.php
@@ -14,8 +14,65 @@
     <title>Computer Science Courses</title>
   </head>
   <body>
-    <h1> Math Courseses</h1>
+    <h1> Math Courses</h1>
     <p>add course things...</p>
+    
+    <?php
+      //print each element
+      include 'readXML.php';
+      
+      //read in list of classes
+      $mathDB = readXML("test.xml");
+
+      //print each class name
+      foreach ($mathDB as $class)
+	  {
+        echo "<p>" . $class["name"] . "</br>" .
+          $class["preFix"] . $class["number"] . "</br>" .
+          "Credits:" . $class["credits"] . "</br>" ;
+              
+         
+          //"Offered:" . $class["offered"] . "</br>" .
+          
+          echo "Description:" . $class["description"] . "</br>" .
+          "Prerequisites: " ;
+
+        //check for no preReqs
+        if (empty($class["preReq"]))
+        {
+            echo "None";
+        } else 
+        {
+            //preint each preReq
+            foreach ($class["preReq"] as $req)
+            {
+                echo $req . " ";
+            }
+        }        
+    
+        echo "</br>Corequisites: ";
+    
+        //check for no coReqs
+        if (empty($class["coReq"]))
+        {
+            echo "None";
+        } else 
+        {
+            //print each coReq
+            foreach ($class["coReq"] as $req)
+            {
+                echo $req . " ";
+            }
+        }
+        
+        echo "</br>Notes: " . $class["notes"] . "</br></p>";  
+        
+              
+            
+	}
+
+    ?>
+
   </body>
 
 </html>
